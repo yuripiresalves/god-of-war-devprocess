db96c1496d944fc4bee75e1270cf9389bf8b325e
Yuri Alves <yuripiresalves@gmail.com>
2022-11-27 14:49:16 -0300

    aula 4

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 const swiper = new Swiper('.slide-characteres', {
-  slidesPerView: 3.5,
   spaceBetween: 19,
   freeMode: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.1,
+    },
+    480: {
+      slidesPerView: 1.2,
+    },
+    560: {
+      slidesPerView: 1.8,
+    },
+    768: {
+      slidesPerView: 2.2,
+    },
+    991: {
+      slidesPerView: 2.8,
+    },
+    1200: {
+      slidesPerView: 3.5,
+    },
+  },
+});
+
+AOS.init({
+  duration: 1000,
+  once: true,
 });
